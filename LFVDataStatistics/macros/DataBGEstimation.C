f57d6417ff7b3b127446ddcf311971eb9e107f5b
avitaldery <avitaldery@weizmann.ac.il>
2014-05-21 15:36:16 +0300

    bla

--- LFVDataStatistics/macros/DataBGEstimation.C
+++ LFVDataStatistics/macros/DataBGEstimation.C
@@ -12,6 +12,7 @@ using namespace std;
 
 void DataBGEstimation(TString filename, TString sigFile)
 {
+	//AVITAL BLAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHh
 	InitExterns();
 
 	int Polydegree = 2;
@@ -31,24 +32,13 @@ void DataBGEstimation(TString filename, TString sigFile)
 
 	double muHatB[nbins+2+Polydegree];
 
-	//trial: add polynomial to one histo
-	double n[nbins+2];
-	for (int i=1; i<=nbins; i++){
-		n[i] = h_ME->GetBinContent(i);
-		double Mcoll = h_ME->GetBinCenter(i);
-		double width = h_ME->GetXaxis()->GetBinWidth(i);
-		n[i] += (0.5 + 0.0002*Mcoll)*width;
-	}
-
-	n[0] = 0; n[nbins+1] = 0;
-
-	TH1D *h_MEplus = new TH1D("hMEplus","hMEplus",nbins, massBins);
-	h_MEplus->SetContent(n);
-
-	TH1D* h_MESignal = ToyData::appendSignal(h_ME,h_sig,5,"hMEsignal");
+	// add polinom on top of histogram
+	double a0=0.1;
+	double a1=0.003;
+	TH1D* h_ME_plus = BkgEstimator::AddPolonHisto(h_ME,a0,a1);
 
 	//get BG estimation
-	TH1D* h_B =  BkgEstimator::meanDataEstimator(h_MESignal,h_EM,muHatB,h_sig,Polydegree,"B");
+	TH1D* h_B =  BkgEstimator::meanDataEstimator(h_ME,h_EM,muHatB,h_sig,Polydegree,"B");
 
 	//create blind histos to draw
 	TH1D* h_ME_blind = ToyData::getBlindHisto(h_ME,8,12,"ME blind");
@@ -62,7 +52,7 @@ void DataBGEstimation(TString filename, TString sigFile)
 	h_ME_blind->SetLineColor(kRed); h_ME_blind->SetLineWidth(2); //h_ME_blind->Draw("e1 sames");
 	h_EM_blind->SetLineColor(kBlue); h_EM_blind->SetLineWidth(2);// h_EM_blind->Draw("e1 sames");
 	h_EM->SetLineColor(kBlue); h_EM->SetLineWidth(2); h_EM->Draw("e1 sames");
-	h_MESignal->SetLineColor(kRed);h_MESignal->SetLineWidth(2); h_MESignal->Draw("sames");
+//	h_MESignal->SetLineColor(kRed);h_MESignal->SetLineWidth(2); h_MESignal->Draw("sames");
 //	h_MEplus->Draw("sames");
 	leg->AddEntry(h_ME_blind,"#mue","l");
 	leg->AddEntry(h_EM_blind,"e#mu","l");
@@ -70,6 +60,6 @@ void DataBGEstimation(TString filename, TString sigFile)
 	leg->Draw();
 
 	//get best polynomial coefficients
-	BkgEstimator::PrintPolyCoefficients(h_ME,h_MEplus,2);
+	BkgEstimator::PrintPolyCoefficients(h_ME,h_EM,2);
 
 }
