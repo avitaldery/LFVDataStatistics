40aad2c7519c37e6ad438ffa77e90caccd31c796
avitaldery <avitaldery@gmail.com>
2014-05-21 17:56:32 +0300

    Delete LFVDataStatistics_C.d

--- LFVDataStatistics/cmt/LFVDataStatistics_C.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./../cmt/LFVDataStatistics_C.so: /root/Programs/root/include/cintdictversion.h /root/Programs/root/include/RVersion.h
-LFVDataStatistics_C__ROOTBUILDVERSION= 5.34/13
